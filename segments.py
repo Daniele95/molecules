1d167142dec5cbc24be5bbc63e81f56d289c23a2
Daniele <danielee.gamba@gmail.com>
2018-04-12 17:40:22 +0200

    created object membrana

--- segments.py
+++ segments.py
@@ -1,8 +1,4 @@
 import matplotlib.pyplot as plt
-import matplotlib.collections as mc
-import numpy as np
-
-#TODO: implementare classi segmentList e pointList con metodo print
 
 class point( object ):
   x = 0
@@ -10,11 +6,6 @@ class point( object ):
   def __init__( self, x, y ):
     self.x = x
     self.y = y
-  def x():
-    return x
-  def y():
-    return x
-	
 	
 class segment( object ):
   p1 = point( 0, 0 )
@@ -22,33 +13,32 @@ class segment( object ):
   def __init__( self, p1, p2 ):
     self.p1 = p1
     self.p2 = p2
-  def p1():
-    return p1
-  def p2():
-    return p2
 
-N = 30
-segmentsRange = range( 0, N )
-pointsRange = range( 0, N+1 )
-
-def f( x ):
-  return 0
-
-segmentList = [ segment( point(i,f(i)), point(i+1,f(i+1)) ) for i in pointsRange ]
-
-#creo anche una lista di punti, che per ora non utilizzo
-pointList = [ segmentList[i].p1 for i in pointsRange ]
-pointList.append(segmentList[N].p2)
-
-s = segmentList
-
-
-for i in pointsRange:
-  plt.plot(segmentList[i].p1.x,segmentList[i].p1.y, 'bo' )
+class membrana( object ):
+  points = []
+  indices = []
+  segments = []
   
-for i in segmentsRange:
-  plt.plot([s[i].p1.x, s[i+1].p1.x], [s[i].p1.y, s[i+1].p1.y], color='k', linestyle='-', linewidth=2)
-  
+  def __init__( self, points, indices ):
+    self.points = points
+    self.indices = indices
+    l = len( points )
+    self.segments = [ segment( points[indices[i]], points[indices[i+1]] ) for i in range ( 0, l-1 ) ]
+	
+  def plot( self ):
+    l = len( points )
+    for i in range ( 0, l ):
+      plt.plot( points[i].x, points[i].y, 'bo' )
+    for i in range( 0, l-1 ):
+      s = self.segments
+      plt.plot( [s[i].p1.x, s[i].p2.x], [s[i].p1.y, s[i].p2.y], color='k', linestyle='-', linewidth=2)
+
+
+points = [point(1,0), point(2,.5), point(3,2),point(5,.1)]
+indices = [0,3,2,1]
+
+m = membrana( points, indices )
+m.plot()
 
 
 plt.xlabel( 'x' )
